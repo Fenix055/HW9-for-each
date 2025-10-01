7b3e71245fa270859cfc3b8f5d90dadc482562fc
Fenix055 <DevilHanter05@gmail.com>
2025-10-01 16:13:36 +0300

    Work is done

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,46 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        /// Task 1
+        short[] paymentDay = new short[]{10_000, 11_000, 12_000, 13_000, 14_000};
+        int sum = 0;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (short i : paymentDay) {
+            sum += i;
         }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+
+        /// Task 2
+        short[] paymentWeek = new short[]{10_000, 11_000, 12_000, 13_000, 14_000};
+        short minWeek = 32_767;
+        short maxWeek = 0;
+
+        for (short i : paymentWeek) {
+            if (i > maxWeek) {
+                maxWeek = i;
+            }
+            if (i < minWeek) {
+                minWeek = i;
+            }
+        }
+        System.out.println("Минимальная сумма трат за неделю составила " + minWeek + " рублей. Максимальная сумма трат за неделю составила " + maxWeek + " рублей");
+
+        /// Task 3
+        short[] paymentWeek2 = new short[]{10_000, 11_000, 12_000, 13_000, 14_000};
+        sum = 0;
+
+        for (short i : paymentWeek2) {
+            sum += i;
+        }
+        System.out.println("Сумма трат за месяц составила " + (float) (sum / paymentWeek2.length) + " рублей");
+
+        /// Task 4
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+
+        for (byte i = 0; i <= reverseFullName.length / 2; i++) {
+            char x = reverseFullName[i];
+            reverseFullName[i] = reverseFullName[reverseFullName.length - 1 - i];
+            reverseFullName[reverseFullName.length - 1 - i] = x;
+        }
+        System.out.println(reverseFullName);
     }
 }
